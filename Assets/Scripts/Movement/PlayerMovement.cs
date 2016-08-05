f5da51e11bb81510796b2c7a20f5617e30ae8ba7
Johannes Wagner <b.johannes.wagner@gmail.com>
2016-08-05 21:16:09 +0200

    testmap, animations and more

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animation))]
 public class PlayerMovement : InputController {
 	[SerializeField] protected float jumpForce;
 	[SerializeField] protected int maxNumberOfJumps = 1;
@@ -10,7 +12,7 @@ public class PlayerMovement : InputController {
 	[SerializeField] private bool reverseSpriteX = false;
 
 	protected Rigidbody2D rigidb;
-	//protected Animator animator;
+	protected Animator animator;
 	protected int availableJumps;
 	protected bool didJump;
 	protected float faceDirection = 1f;
@@ -19,7 +21,7 @@ public class PlayerMovement : InputController {
 	protected override void Awake() {
 		base.Awake();
 		rigidb = GetComponent<Rigidbody2D>();
-		//animator = GetComponent<Animator>();
+		animator = GetComponent<Animator>();
 		availableJumps = 0;
 		didJump = false;
 		reverseModX = reverseSpriteX ? -1f : 1f;
@@ -34,7 +36,7 @@ public class PlayerMovement : InputController {
 	// FixedUpdate is called once per timeinterval (Edit->ProjSetting->Time), use for Physics
 	void FixedUpdate() {
 		// modify available jumps if player falls of edge, etc...
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
 		if (hit.collider == null && availableJumps == maxNumberOfJumps) {
 			availableJumps = maxNumberOfJumps - 1;
 		}
@@ -49,11 +51,11 @@ public class PlayerMovement : InputController {
 		}
 
 		if (Mathf.Abs(inputHorizontal) > 0) {
-			// animator.SetBool("isWalking", true);
+			animator.SetBool("isWalking", true);
 			faceDirection = inputHorizontal > 0 ? 1 : -1;
 			faceDirection *= reverseModX;
 		} else {
-			// animator.SetBool("isWalking", false);
+			animator.SetBool("isWalking", false);
 		}
 
 		transform.localScale = new Vector3(faceDirection, 1, 1);
@@ -62,7 +64,7 @@ public class PlayerMovement : InputController {
 	// handle Collision with Ground and restore available jumps
 	void OnCollisionEnter2D(Collision2D collision) {
 		//TODO: shorten the raycast so player cant jump when rolling over a hill and in air
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
 
 		if (hit.collider != null) {
 			availableJumps = maxNumberOfJumps;
