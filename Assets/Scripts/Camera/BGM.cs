25adb292daa1049e7b4df9ab3e563b7dd19999b3
Johannes Wagner <b.johannes.wagner@gmail.com>
2016-08-06 16:53:08 +0200

    BGM script

--- Assets/Scripts/Camera/BGM.cs
+++ Assets/Scripts/Camera/BGM.cs
@@ -2,15 +2,27 @@
 using System.Collections;
 
 public class BGM : MonoBehaviour {
-	[SerializeField] protected AudioClip lifeBGM;
-	[SerializeField] protected AudioClip dreamBGM;
+	[SerializeField] protected GameObject lifeBGM;
+	[SerializeField] protected GameObject dreamBGM;
+	[SerializeField] protected float fadeTime;
+
+	private AudioSource lifeSource;
+	private AudioSource dreamSource;
 	// Use this for initialization
 	void Start () {
-	
+		lifeSource = lifeBGM.GetComponent<AudioSource>();
+		dreamSource = dreamBGM.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (dreamSource.volume < 1) {
+			dreamSource.volume += fadeTime * Time.deltaTime;
+		}
+
+		if (lifeSource.volume > 0) {
+			lifeSource.volume -= fadeTime * Time.deltaTime;
+		}
 	}
 }
