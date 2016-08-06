58ddab2807cbbbb671e3f9d3eff9cd0319dbbb79
Johannes Wagner <b.johannes.wagner@gmail.com>
2016-08-06 16:15:24 +0200

    music

---
+++ Assets/Scripts/Camera/BGM.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour {
+	[SerializeField] protected AudioClip lifeBGM;
+	[SerializeField] protected AudioClip dreamBGM;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
