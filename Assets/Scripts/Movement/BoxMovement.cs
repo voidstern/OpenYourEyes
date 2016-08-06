606cd95eab1839b3cba769a380e9f580a66edc18
Johannes Wagner <b.johannes.wagner@gmail.com>
2016-08-06 23:05:38 +0200

    expanded level and more

---
+++ Assets/Scripts/Movement/BoxMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class BoxMovement : MonoBehaviour {
+	[SerializeField] private GameObject camera;
+
+	private Rigidbody2D rigidb;
+
+	// Use this for initialization
+	void Start () {
+		rigidb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(camera.GetComponent<Grayscale>().enabled) {
+			rigidb.constraints = RigidbodyConstraints2D.FreezeAll;
+		} else {
+			rigidb.constraints = RigidbodyConstraints2D.FreezeRotation;
+		}
+	}
+}
