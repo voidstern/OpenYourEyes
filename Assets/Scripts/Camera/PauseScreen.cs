9233533ad60576b0484b859bac60628bebc6c6ce
stefanreichenauer <stefan-reichenauer@gmx.at>
2016-08-06 18:28:47 +0200

    Added PauseMenu

---
+++ Assets/Scripts/Camera/PauseScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.ImageEffects;
+
+public class PauseScreen : MonoBehaviour {
+    public Blur blur;
+    public Canvas pauseScreen;
+    // Use this for initialization
+    void Start () {
+        blur.enabled = false;
+        pauseScreen.enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+	}
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        blur.enabled = true;
+        pauseScreen.enabled = true;
+    }
+
+    public void ResumeGame ()
+    {
+        blur.enabled = false;
+        pauseScreen.enabled = false;
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
